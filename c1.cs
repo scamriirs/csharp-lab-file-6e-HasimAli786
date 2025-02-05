5ae14fd12a14f64fc93efee022e390dbbdd71b3f
Hasim Ali <160258934+HasimAli786@users.noreply.github.com>
2025-02-05 12:31:16 +0530

    Create c1.cs

---
+++ c1.cs
@@ -0,0 +1,54 @@
+// ParameterLess
+using System;
+
+public class Student  
+{
+    int studentid;
+    string studentname;
+    public Student()  
+    {
+        studentid = 0;
+        studentname = "abc";
+    }
+    public static void Main()
+    {
+        Student s1 = new Student();  
+        Console.WriteLine(s1.studentid);   
+        Console.WriteLine(s1.studentname); 
+    }
+}
+
+// Parameterised
+using System;
+public class Student
+{
+    int studentid;
+    string studentname;
+    public Student(int id, string s)
+    {
+        studentid = id;
+        studentname = s;
+    }
+    public static void Main()
+    {
+        Student s1 = new Student(2, "Ram");
+        Console.WriteLine(s1.studentid);
+        Console.WriteLine(s1.studentname);
+    }
+}
+
+// Default
+using System;
+
+public class Student
+{
+    int studentid;        
+    string studentname;   
+
+    public static void Main()
+    {
+        Student s1 = new Student(); 
+        Console.WriteLine(s1.studentid);    
+        Console.WriteLine(s1.studentname); 
+    }
+}
